85d9808746d8678da3e2e973848b1124e83a917b
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-10-03 15:54:43 -0400

    Gender option

--- app.js
+++ app.js
@@ -9,6 +9,34 @@ angular.module("myApp", []).controller("myController", ["$scope",
 			$scope.color = "black";
 			$scope.mount = "destrier";
 			$scope.priority = "wardrobe";
+			
+			$scope.gender = 'Male';
+			$scope.child = 'son';
+			$scope.pronounUpper = 'He';
+			$scope.pronounLower = 'he';
+			$scope.possessivePronoun = 'his';
+			$scope.sibling = 'Brother';
+
+			$scope.genderApply = function(gender) {
+				if(gender === 'Male') {
+					$scope.gender = 'Male';
+					$scope.child = 'son';
+					$scope.pronounUpper = 'He';
+					$scope.pronounLower = 'he';
+					$scope.possessivePronoun = 'his';
+					$scope.sibling = 'Brother';				
+					console.log('son fired');
+				} else {
+					$scope.gender = 'Female';
+					$scope.child = 'daughter';
+					$scope.pronounUpper = 'She';
+					$scope.pronounLower = 'she';
+					$scope.possessivePronoun = 'her';
+					$scope.sibling = 'Sister';
+					console.log('daughter fired');
+				}
+				console.log('function method a fired');
+			};
 		}
 	]);
 
