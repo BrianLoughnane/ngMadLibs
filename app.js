986b38984635f0ba58b0492654abe3546d9202bb
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-10-03 15:01:14 -0400

    Controller Refactor
    Got rid of ng-init and set initial models based on an externally defined
    controller

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module("myApp", []).controller("myController", ["$scope", 
+		function($scope) {
+			$scope.name = "Ser Waymar Royce";
+			$scope.place = "house";
+			$scope.inhabitants = "heirs";
+			$scope.descriptor = "handsome";
+			$scope.age = "eighteen";
+			$scope.comparator = "knife";
+			$scope.color = "black";
+			$scope.mount = "destrier";
+			$scope.priority = "wardrobe";
+		}
+	]);
+
